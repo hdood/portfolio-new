bbf70e4f4e8b8bdf8e6c9a65762f9dbb52bfecf1
Mahdi Bouguerzi <m.bouguerzi1234@gmail.com>
2026-07-22 18:57:40 +0100

    add vercel statistics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,8 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxt/content",
     "@nuxthq/studio",
-    "@vueuse/nuxt"
+    "@vueuse/nuxt",
+    "@vercel/analytics"
   ],
   ui: {
     icons: ["heroicons", "lucide"],
